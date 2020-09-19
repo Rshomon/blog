4168565f70b4a951e8a8ce2a51bd25bacb61717b
Rashomon <793652230@qq.com>
2020-09-19 15:09:05 +0800

    update index page

--- src/Test/Test.jsx
+++ src/Test/Test.jsx
@@ -5,29 +5,29 @@ import "./Test.less"
 
 export default class Test extends React.Component {
 
-    componentDidMount() {
-        window.addEventListener('scroll', this.handleScroll.bind(this)) //监听滚动
-        window.addEventListener('resize', this.handleResize.bind(this)) //监听窗口大小改变
-    }
+    // componentDidMount() {
+    //     window.addEventListener('scroll', this.handleScroll.bind(this)) //监听滚动
+    //     window.addEventListener('resize', this.handleResize.bind(this)) //监听窗口大小改变
+    // }
 
-    componentWillUnmount() { //一定要最后移除监听器，以防多个组件之间导致this的指向紊乱
-        window.removeEventListener('scroll', this.handleScroll.bind(this))
-        window.removeEventListener('resize', this.handleResize.bind(this))
-    }
+    // componentWillUnmount() { //一定要最后移除监听器，以防多个组件之间导致this的指向紊乱
+    //     window.removeEventListener('scroll', this.handleScroll.bind(this))
+    //     window.removeEventListener('resize', this.handleResize.bind(this))
+    // }
 
-    handleScroll = e => {
-        console.log(
-            '浏览器滚动事件',
-            e.srcElement.scrollingElement.scrollTop,
-            e.srcElement.scrollingElement.scrollHeight
-        )
-        //e.srcElement.scrollingElement.scrollTop为距离滚动条顶部高度
-        // e.srcElement.scrollingElement.scrollHeight为整个文档高度
-    }
+    // handleScroll = e => {
+    //     console.log(
+    //         '浏览器滚动事件',
+    //         e.srcElement.scrollingElement.scrollTop,
+    //         e.srcElement.scrollingElement.scrollHeight
+    //     )
+    //     //e.srcElement.scrollingElement.scrollTop为距离滚动条顶部高度
+    //     // e.srcElement.scrollingElement.scrollHeight为整个文档高度
+    // }
 
-    handleResize = e => {
-        console.log('浏览器窗口大小改变事件', e.target.innerWidth)
-    }
+    // handleResize = e => {
+    //     console.log('浏览器窗口大小改变事件', e.target.innerWidth)
+    // }
     render() {
         return (
             <div className="wrapper">
--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -6,18 +6,25 @@ class Index extends React.Component {
         return (
             <div className="root" style={{ fontSize: '16px', fontFamily: '"Helvetica Neue",Helvetica,Arial,sans-serif' }}>
                 <div className="head">
-                    <div className="left-head"><h3>Cover</h3></div>
+                    <div className="left-head"><h3><a href="#">Blog</a></h3></div>
                     <div className="right-head">
                         <ul className="right-list">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Features</a></li>
-                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">主页</a></li>
+                            <li><a href="#">博客</a></li>
+                            <li><a href="#">关于</a></li>
                         </ul>
                     </div>
                 </div>
-                <div className="content">我是内容</div>
-                <div className="footer">我是底部</div>
+                <div className="content">
+                    <div className="content-head"><h1 className="at-item">Rm的个人博客</h1></div>
+                    <div className="content-main">不爱摄影的程序员不会是好厨子</div>
+                    <div className="content-footer">
+                        <a href="/home" type="button">进入</a>
+                    </div>
+                </div>
+                <div className="footer">Cover template for <a href='#'>Bootstrap</a>, by <a href='#'>@Rm</a>.</div>
             </div>
+
         )
     }
 }
